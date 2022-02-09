77be9c7363ce96359250c7419624e2ae314eaf72
Erlan Isakov <erlanisakov67@gmail.com>
2022-02-10 00:02:26 +0600

    project

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.burger').on('click',function () {
+        $('.header__menu').toggleClass('header__menu_active')
+        $('.burger').toggleClass('open')
+    })
+});
